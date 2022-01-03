83b6bd45ff172494398084fc6b6114f8d3b0ef60
Mahfod <mahfod.dev@gmail.com>
2022-01-03 19:06:55 +0100

    connect mongoDB and change commonjs import module

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 
 const app = express()
 
-
-app.get('/',(req,res)=>{
-    res.send('API IS RUNNING')
+app.get('/', (req, res) => {
+	res.send('API IS RUNNING')
 })
-app.get('/api/products',(req,res)=>{
-    res.json(products)
+app.get('/api/products', (req, res) => {
+	res.json(products)
 })
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find(p => p._id === req.params.id )
-    res.json(product)
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id)
+	res.json(product)
 })
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
